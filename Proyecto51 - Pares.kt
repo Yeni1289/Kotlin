349084a7d7cd0a5a2b4b2f268414f0861fba06fd
Yeni1289 <154487288+Yeni1289@users.noreply.github.com>
2025-06-19 23:31:55 -0600

    Create Proyecto51 - Pares.kt

---
+++ Proyecto51 - Pares.kt
@@ -0,0 +1,13 @@
+
+fun main(parametros: Array<String>) {
+ var cant = 0
+ print("Cuantos valores ingresará para analizar:")
+ val cantidad = readln().toInt()
+ for(i in 1..cantidad) {
+ print("Ingrese valor:")
+ val valor = readln().toInt()
+ if (valor % 2 ==0)
+ cant++
+ }
+ println("Cantidad de pares: $cant")
+}
