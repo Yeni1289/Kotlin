7ece13e2bab07d1a44e7a4f6cbabaeeebbd98dd2
Yeni1289 <154487288+Yeni1289@users.noreply.github.com>
2025-06-23 22:43:55 -0600

    Create Proyecto59 - Principal.kt

---
+++ Proyecto59 - Principal.kt
@@ -0,0 +1,14 @@
+
+fun main(parametro: Array<String>) {
+ print("Ingrese coordenada x del punto:")
+ val x = readln().toInt()
+ print("Ingrese coordenada y del punto:")
+ val y = readln().toInt()
+ when {
+ x > 0 && y > 0 -> println("Primer cuadrate")
+ x < 0 && y > 0 -> println("Segundo cuadrante")
+ x < 0 && y < 0 -> println("Tercer cuadrante")
+ x > 0 && y < 0 -> println("Cuarto cuadrante")
+ else -> println("El punto se encuentra en un eje")
+ }
+}
